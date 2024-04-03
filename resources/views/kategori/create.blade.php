0a743bd47c5225f09493b947a71048f8c8e42193
Sony Febri Hari Wibowo <sonywib04@gmail.com>
2024-04-04 00:03:10 +0700

    add validation

--- resources/views/kategori/create.blade.php
+++ resources/views/kategori/create.blade.php
@@ -1,27 +1,43 @@
 @extends('layout.app')
 
-{{-- customize layout section --}}
-
+{{-- customize layout sections --}}
 @section('subtitle', 'Kategori')
 @section('content_header_title', 'Kategori')
 @section('content_header_subtitle', 'Create')
-{{-- Main page Content --}}
+{{-- content body: main page content --}}
 @section('content')
     <div class="container">
         <div class="card card-primary">
             <div class="card-header">
-                <h3 class="card-title">Buat kategori baru</h3>
+                <h3 class="card-title">Buat Kategori Baru</h3>
             </div>
 
-            <form action="../kategori" method="post">
+            <form action="../kategori" method="POST">
                 <div class="card-body">
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->category->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
                     <div class="form-group">
                         <label for="kodeKategori">Kode Kategori</label>
-                        <input type="text" class="form-control" name="kodeKategori" id="kodeKategori" placeholder="Masukkan Kode Kategori..">
+                        <input type="text" class="form-control @error('kategori_kode') is-invalid @enderror" id="kategori_kode" name="kategori_kode"
+                            placeholder="Kode Kategori">
+                        @error('kategori_kode', 'category')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
                     </div>
                     <div class="form-group">
                         <label for="namaKategori">Nama Kategori</label>
-                        <input type="text" class="form-control" name="namaKategori" id="namaKategori" placeholder="Masukkan Nama Kategori..">
+                        <input type="text" class="form-control @error('kategori_nama') is-invalid @enderror" id="kategori_nama" name="kategori_nama"
+                            placeholder="Nama Kategori">
+                        @error('kategori_nama', 'category')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
                     </div>
                 </div>
 
