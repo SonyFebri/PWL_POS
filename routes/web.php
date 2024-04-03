990af45f6c7b87a4eb7fcb6428e6976205389297
Sony Febri Hari Wibowo <sonywib04@gmail.com>
2024-04-04 02:32:47 +0700

    extract admin LTE

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,8 @@
 
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
-use App\Http\Controllers\UserController;
 use App\Http\Controllers\POSController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,29 +17,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/kategori', [KategoriController::class, 'index']);
+
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/level', [LevelController::class, 'index']);
-Route::get('/level/add', [LevelController::class, 'add'])->name('/level/add');
-Route::post('/level/add_save', [LevelController::class, 'add_save'])->name('/level/add_save');
-
-Route::get('/kategori', [KategoriController::class, 'index']);
-Route::get('/user', [UserController::class, 'index'])->name('/user');
-
-Route::get('/user/add', [UserController::class, 'add'])->name('/user/add');
-Route::get('/user/update/{id}', [UserController::class, 'update'])->name('/user/update');
-Route::get('/user/delete/{id}', [UserController::class, 'delete'])->name('/user/delete');
-
-Route::post('/user/add_save', [UserController::class, 'add_save'])->name('/user/add_save');
-
-Route::put('/user/update_save/{id}', [UserController::class, 'update_save'])->name('/user/update_save');
-
-Route::get('/kategori/create', [KategoriController::class, 'create'])->name('kategori.create');
+Route::get('/level/tambah', [LevelController::class, 'tambah']);
 Route::post('/kategori', [KategoriController::class, 'store']);
-Route::get('/kategori/update/{id}', [KategoriController::class, 'update'])->name('kategori.update');
-Route::put('/kategori/update_save/{id}', [KategoriController::class, 'update_save'])->name('kategori.update_save');
-Route::get('/kategori/delete/{id}', [KategoriController::class, 'delete'])->name('kategori.delete');
+Route::get('/kategori/create', [KategoriController::class, 'create']);
+Route::get('/kategori/edit/{id}', [KategoriController::class, 'edit'])->name('kategori.edit');
+Route::put('/kategori/edit_simpan/{id}', [KategoriController::class, 'edit_simpan']);
+Route::get('/kategori/hapus/{id}', [KategoriController::class, 'hapus'])->name('kategori.hapus');
+
+Route::get('/user', [UserController::class, 'index']);
+Route::get('/user/tambah', [UserController::class, 'tambah']);
+Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
+Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
+Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
+Route::get('user/hapus/{id}', [UserController::class, 'hapus']);
 
 Route::resource('m_user', POSController::class);
